81cf7397cdb1039326090ef1c05efb59c0a25dbf
FatimaAflous <aflousfatima@gmail.com>
2024-12-14 11:55:16 +0100

    add security to Gateway

--- Service-Gateway/src/main/java/com/projet_restaurant/servicegateway/ServiceGatewayApplication.java
+++ Service-Gateway/src/main/java/com/projet_restaurant/servicegateway/ServiceGatewayApplication.java
@@ -1,13 +1,16 @@
 package com.projet_restaurant.servicegateway;
 
+import com.projet_restaurant.servicegateway.configuration.RsaConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
 import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
 import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 @SpringBootApplication
+@EnableConfigurationProperties(RsaConfig.class)
 public class ServiceGatewayApplication {
 
     public static void main(String[] args) {
--- Service-Gateway/src/main/java/com/projet_restaurant/servicegateway/configuration/SecurityConfig.java
+++ Service-Gateway/src/main/java/com/projet_restaurant/servicegateway/configuration/SecurityConfig.java
@@ -4,9 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtException;
 import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
 import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -19,17 +22,29 @@ public class SecurityConfig {
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         http
                 .csrf(csrf -> csrf.disable())
-                .authorizeExchange(auth -> auth.pathMatchers("/ENSEIGNANT-SERVICE/Enseignants/email/{id}").permitAll())
-                .authorizeExchange(auth -> auth.pathMatchers("/CHERCHEUR-SERVICE/Chercheurs/email/{id}").permitAll())
-                .authorizeExchange(auth -> auth.pathMatchers("/CHERCHEUR-SERVICE/Chercheurs").hasAuthority("Chercheur"))
-                         .authorizeExchange(auth -> auth.pathMatchers("/ENSEIGNANT-SERVICE/Enseignants/**").hasAuthority("SCOPE_Enseignant"))
-                         .authorizeExchange(auth -> auth.anyExchange().authenticated())
+                .authorizeExchange(auth -> auth.pathMatchers("/SERVICE-UTILISATEURS/login").permitAll())
+                .authorizeExchange(auth -> auth.pathMatchers("/MENU-SERVICE/graphql").permitAll()/*.hasAnyAuthority("ADMIN", "CLIENT")*/)
+                .authorizeExchange(auth -> auth.anyExchange().authenticated())
                          .oauth2ResourceServer(oauth2 -> oauth2.jwt());
                          return http.build();
                          }
-                         @Bean
-                         public ReactiveJwtDecoder jwtDecoder() {
-                         // Ensure that rsaConfig.publicKey() returns a valid PublicKey
-                         return NimbusReactiveJwtDecoder.withPublicKey(rsaConfig.publicKey()).build();
-                         }
-                         }
+
+
+    @Bean
+    public ReactiveJwtDecoder jwtDecoder() {
+        NimbusReactiveJwtDecoder jwtDecoder = NimbusReactiveJwtDecoder.withPublicKey(rsaConfig.publicKey()).build();
+
+        return new ReactiveJwtDecoder() {
+            @Override
+            public Mono<Jwt> decode(String token) throws JwtException {
+                return jwtDecoder.decode(token)
+                        .doOnNext(jwt -> {
+                            System.out.println("Decoded JWT: " + jwt.getClaims());
+                            System.out.println("Scope: " + jwt.getClaimAsString("scope"));
+                        })
+                        .doOnError(error -> System.err.println("Error decoding JWT: " + error.getMessage()));
+            }
+        };
+    }
+
+}
