a21a2b5bb1681227fd022f8202349b3fc66451e1
FatimaAflous <aflousfatima@gmail.com>
2024-12-23 00:43:23 +0100

    Manage payment with Stripe

--- Service-Payment/app.py
+++ Service-Payment/app.py
@@ -1,9 +1,20 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify , request
 from py_eureka_client.eureka_client import EurekaClient
-
+from app.services.rabbitmq_consumer import payment_routes
+from flask_cors import CORS  # Importez CORS
 app = Flask(__name__)
+# Applique CORS à l'application pour toutes les routes et autorise tous les en-têtes et méthodes
+CORS(app, origins="http://localhost:4200", methods=["GET", "POST", "OPTIONS"], allow_headers=["Content-Type", "Authorization"])
 
-# Configuration de Eureka Client
+# Gestion de la demande OPTIONS
+@app.before_request
+def handle_options_request():
+    if request.method == "OPTIONS":
+        response = jsonify({'message': 'CORS Pre-flight response'})
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+        response.headers.add('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+        return response# Configuration de Eureka Client
 from app.config.eureka_config import eureka_client
 
 # Endpoint Health Check
@@ -11,7 +22,9 @@ from app.config.eureka_config import eureka_client
 def health_check():
     return jsonify({"status": "UP"}), 200
 
-from app.services.rabbitmq_consumer import start_consuming
+# Enregistrer le Blueprint de paiement
+app.register_blueprint(payment_routes, url_prefix='/payment')
+
 if __name__ == '__main__':
-    start_consuming()
     app.run(debug=True)
+CORS(app)
--- Service-Payment/app/routes/payment_routes.py
+++
@@ -1,3 +0,0 @@
-from flask import request, jsonify
-from app.services.payment_service import process_payment_with_stripe
-from app import app
