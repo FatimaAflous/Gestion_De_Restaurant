b341662e558bbb8a5fe47b37f041dc60b7a4aa28
Hajar wafiq <wafiqhajaar@gmail.com>
2024-12-27 17:11:39 +0100

    Ajout des tests unitaires

---
+++ Menu-Service/src/test/java/org/example/menuservice/service/MenuServiceTest.java
@@ -0,0 +1,35 @@
+package org.example.menuservice.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class MenuServiceTest {
+
+    @Test
+    void getAllMenus() {
+    }
+
+    @Test
+    void fallbackMenuService() {
+    }
+
+    @Test
+    void getMenusByCategory() {
+    }
+
+    @Test
+    void getMenuById() {
+    }
+
+    @Test
+    void addMenu() {
+    }
+
+    @Test
+    void updateMenu() {
+    }
+
+    @Test
+    void deleteMenu() {
+    }
+}
